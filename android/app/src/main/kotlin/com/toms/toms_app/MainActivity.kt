0d01c3c0ad531e665a19b5f3bd8037adcbb8a887
Admin <ogundijotomilayo501@gmail.com>
2024-02-24 21:45:01 -0800

    how to use GirdView

---
+++ android/app/src/main/kotlin/com/toms/toms_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.toms.toms_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,241 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, });
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white
+      ),
+      child:GridView.count(
+          crossAxisCount: 3,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        children: [
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold
+
+              ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+          TextButton(
+              onPressed: (){},
+              child: Text("Click",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+
+                ),
+              )
+          ),
+        ],
+      )
+    );
+  }
+}
